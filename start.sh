0ac8316dd83c1a58be2527c53a17e5371b9e536a
Xiang Gao <xianggao@Xiangs-MacBook-Pro.local>
2016-05-07 12:14:41 -0700

    change laptop db path

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ if [ `uname` == "Linux" ]; then
     DB_DIR="/mnt/MYUSB/NGenDB/DATA"
     dbcmd="sudo /opt/mongo/bin/mongod --dbpath $DB_DIR --port $DB_PORT --fork --logpath `pwd`/mongodb.log"
 elif [ `uname` == "Darwin" ]; then
-    DB_DIR="/Volumes/MYUSB/NGenDB/DATA"
+    DB_DIR="/Volumes/MyPassport/NGenDB/DATA"
     dbcmd="`pwd`/db/mongod --dbpath $DB_DIR --port $DB_PORT --fork --logpath `pwd`/mongodb.log"
 fi
 
